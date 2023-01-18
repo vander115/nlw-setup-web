a5006f4b40ecdf68dc2ecc3e56751c1a725041b8
Vanderlei Furtuna <vander@pop-os.localdomain>
2023-01-17 22:44:04 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./styles/global.css";
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+export default App;
