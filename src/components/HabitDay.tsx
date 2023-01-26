76e11bb50af29eb824c8ebd69e6b04f2697832ba
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2023-01-26 08:06:41 -0300

    feat: add api data in popover habit day

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,9 +1,8 @@
 import * as Popover from '@radix-ui/react-popover';
-import * as Checkbox from '@radix-ui/react-checkbox';
 import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
-import { Check } from 'phosphor-react';
 import dayjs from 'dayjs';
+import { HabitsList } from './HabitsList';
 
 interface IHabitDayProps {
   completed?: number;
@@ -47,19 +46,7 @@ export function HabitDay({ amount = 0, completed = 0, date }: IHabitDayProps) {
 
           <ProgressBar progress={completedPercentage} />
 
-          <div className="mt-6 flex flex-col gap-3">
-            <Checkbox.Root className="flex items-center gap-3 group">
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
-
-              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitsList date={date} />
         </Popover.Content>
       </Popover.Portal>
     </Popover.Root>
